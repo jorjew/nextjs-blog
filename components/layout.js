54a214552b5199d36f2727aec3cfdd1ca977f7b0
“jorjew <66022792up.ac.th>
2024-12-26 14:24:34 +0700

    p

--- components/layout.js
+++ components/layout.js
@@ -6,7 +6,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'jewyayyay';
+const name = 'jew phongsakron';
 export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }) {
